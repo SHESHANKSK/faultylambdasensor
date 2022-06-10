ee2453faf7af1dd12e92e3ae73e7dcd0f6b54ac7
SHESHANKSK <sheshankkindalkar@gmail.com>
2022-06-10 19:48:10 +0530

    Using Json

--- faultylambdasensor/__init__.py
+++ faultylambdasensor/__init__.py
@@ -1,2 +1,17 @@
-from operator import imod
-from main import *
+from fulemap import fuleMap
+import json
+from ignitionmap import ignition_maps
+fulemap = fuleMap
+for engineModel, load, rpm, afr in fulemap.items():
+    if engineModel == "YAHAMA":
+        print(load)
+        print(rpm)
+        print(afr)
+
+
+def airfuleratio():
+    pass
+
+
+def sparkangle():
+    pass
