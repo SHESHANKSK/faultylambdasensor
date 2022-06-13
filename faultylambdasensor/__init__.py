64203b25158ea922269e40f7e7af0da9993563ab
SHESHANKSK <sheshankkindalkar@gmail.com>
2022-06-13 21:09:18 +0530

    Initial Setup

--- faultylambdasensor/__init__.py
+++ faultylambdasensor/__init__.py
@@ -1,17 +1,20 @@
-from fulemap import fuleMap
-import json
-from ignitionmap import ignition_maps
-fulemap = fuleMap
-for engineModel, load, rpm, afr in fulemap.items():
-    if engineModel == "YAHAMA":
-        print(load)
-        print(rpm)
-        print(afr)
+from fulemap import fule_load, fule_rpm, afr
+from ignitionmap import igni_load, igni_rpm, spark_angle
+from scipy.interpolate import interp2d
 
 
-def airfuleratio():
-    pass
+def airfuleratio(given_load, given_rpm):
+    fu_car = interp2d(fule_load, fule_rpm, afr, kind='linear', fill_value='-1')
+    y = float(round(fu_car(given_load, given_rpm)[0], 4))
+    x = 14.7
+    airistofuleratio = y/x
+    return airistofuleratio
 
 
-def sparkangle():
-    pass
+def sparkangle(given_load, given_rpm):
+    fu_car = interp2d(igni_load, igni_rpm, spark_angle,
+                      kind='linear', fill_value='-1')
+    y = float(round(fu_car(given_load, given_rpm)[0], 4))
+    x = 14.7
+    sparkangle = y/x
+    return sparkangle
