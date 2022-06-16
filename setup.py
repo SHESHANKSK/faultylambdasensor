66a0bdf3b2cf52635d710a444afe5a7bfa15a683
SHESHANKSK <sheshankkindalkar@gmail.com>
2022-06-16 12:01:29 +0530

    Added readme

--- setup.py
+++ setup.py
@@ -10,16 +10,20 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 VERSION = '0.0.3'
 DESCRIPTION = 'Get Air:Fuel Ratio or Spark Angle for Any Engine'
 LONG_DESCRIPTION = 'Python Package for determining the Air: Fuel Ratio from the Fuel Maps and Spark Angle from the Ignition Map for the Faulty Lambda Sensor'
+PROJECT_URLS = {
+    'Source Code': 'https://github.com/SHESHANKSK/faultylambdasensor',
+    'BUG Tracker': 'https://github.com/SHESHANKSK/faultylambdasensor/issues'
+}
 
 # Setting up
 setup(
-    name="faultylambdasensor",
     version=VERSION,
     author="Sheshank Kindalkar",
     author_email="sheshankkindalkar@gmail.com",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
+    project_urls=PROJECT_URLS,
     packages=find_packages(),
     install_requires=['numpy', 'scipy'],
     keywords=['python', 'lambdasensor', 'air:fule',
